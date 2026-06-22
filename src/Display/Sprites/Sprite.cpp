b80e475838ab8d6dab90ddea97b62393fa3b130f
perat1 <tuomo.pera@aalto.fi>
2026-06-22 11:51:29 +0300

    full sprite0

--- src/Display/Sprites/Sprite.cpp
+++ src/Display/Sprites/Sprite.cpp
@@ -5,25 +5,41 @@
 #include "Gba-Graphics/Tile/Tile.h"
 #include "Memory/memoryMappedIO.h"
 #include "Memory/memoryOps.h"
+#include <iostream>
 
 extern RgbaPalette PaletteColours;
 
+// constexpr std::pair<uint8_t, uint8_t> shapes[3][4] = {
+//     {{8, 8}, {16, 16}, {32, 32}, {64, 64}},
+//     {{16, 8}, {32, 8}, {32, 16}, {64, 32}},
+//     {{8, 16}, {8, 32}, {16, 32}, {32, 64}}};
+
 constexpr std::pair<uint8_t, uint8_t> shapes[3][4] = {
-    {{8, 8}, {16, 16}, {32, 32}, {64, 64}},
-    {{16, 8}, {32, 8}, {32, 16}, {64, 32}},
-    {{8, 16}, {8, 32}, {16, 32}, {32, 64}}};
+    {{1, 1}, {2, 2}, {4, 4}, {8, 8}},
+    {{2, 1}, {4, 1}, {4, 2}, {8, 4}},
+    {{1, 2}, {1, 4}, {2, 4}, {4, 8}}};
 
 const auto& Sprite::create1DSprite(SpriteTileset_t& sprite_tiles)
 {
-    ObjReg1* objr1 = reinterpret_cast<ObjReg1*>(oamRam.getMemoryPtr());
-    ObjReg2* objr2 = reinterpret_cast<ObjReg2*>(oamRam.getMemoryPtr() + 2);
-    auto dimensions = shapes[objr1->shape][objr1->size];
+    const ObjReg1* objr1 =
+        reinterpret_cast<ObjReg1*>(oamRam.getMemoryPtr() + m_index * 8);
+    const ObjReg2* objr2 =
+        reinterpret_cast<ObjReg2*>(oamRam.getMemoryPtr() + m_index * 8 + 4);
+    const auto dimensions = shapes[objr1->shape][objr1->size];
 
-    const auto tile = sprite_tiles.linear[512].create(16, false, false, false);
+    const uint32_t tile_start = objr2->tileNumber;
+    for (size_t i = 0; i < dimensions.first * dimensions.second; i++) {
+        const auto& tile = sprite_tiles.linear[tile_start + i].create(
+            16 + objr2->paletteNumber, false, false, false);
+        const uint32_t y_offset = (i / 8);
+        const uint32_t x_offset = (i % (dimensions.first));
 
-    for (size_t x = 0; x < 8; x++) {
-        for (size_t y = 0; y < 8; y++) {
-            m_pixels[y][x] = tile.grid[y][x];
+        std::cout << y_offset << " " << x_offset << "\n";
+
+        for (size_t x = 0; x < 8; x++) {
+            for (size_t y = 0; y < 8; y++) {
+                m_pixels[y_offset * 8 + y][x_offset * 8 + x] = tile.grid[y][x];
+            }
         }
     }
 
